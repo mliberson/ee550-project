16b132f723ed2920f415adeab9ec186c25583839
mliberson <mliberso@usc.edu>
2023-04-26 21:26:50 -0700

    upload

--- projectDavidMATPLOTLIB.py
+++ projectDavidMATPLOTLIB.py
@@ -30,10 +30,9 @@ class Node:
         for i in range(len(self.Edges)):
             if random.random() < self.Edges[i] and not i == self.ID:
                 options.append(i)
-                packetID = self.Queue[-1]
+                packetID = self.Queue[0]
         return options, packetID
     def RemovePacket(self):
-        self.Queue.pop()
         self.QueueSize = self.QueueSize - 1
     def InsertPacket(self, packetID):
         if packetID:
@@ -41,9 +40,20 @@ class Node:
             self.QueueSize = self.QueueSize + 1
 
 class Graph:
-    def __init__(self, queueSelection: bool):
+    def __init__(self, edges, queueSelection: bool):
         self.Nodes = []
-        
+        self.Edges = edges
+
+        self.Edges = [[1,0.4,0.4,0.4,0,0,0,0,0],
+                      [0,1,0,0,0.4,0.4,0,0,0],
+                      [0,0,1,0.4,0.4,0.4,0,0,0],
+                      [0,0,0,1,0,0,0,0.4,0],
+                      [0,0,0,0,1,0,0.4,0.4,0],
+                      [0,0,0,0,0,1,0.4,0.4,0.4],
+                      [0,0,0,0,0,0,1,0,0.4],
+                      [0,0,0,0,0,0,0,1,0.4],
+                      [0,0,0,0,0,0,0,0,1]]
+        '''
         self.Edges = [[1, 0.4, 0, 0.4, 0, 0, 0],
                       [0, 1, 0.4, 0.4, 0.1, 0, 0],
                       [0, 0, 1, 0, 0.4, 0, 0],
@@ -52,7 +62,6 @@ class Graph:
                       [0, 0, 0, 0, 0, 1, 0.4],
                       [0, 0, 0, 0, 0, 0, 1]]
         
-        '''
         self.Edges = [[1,0.5,0.5,0.5,0,0,0,0,0,0,0],
                       [0,1,0,0,0.5,0.5,0.5,0,0,0,0],
                       [0,0,1,0,0.5,0.5,0.5,0,0,0,0],
@@ -65,6 +74,7 @@ class Graph:
                       [0,0,0,0,0,0,0,0,0,1,0.5],
                       [0,0,0,0,0,0,0,0,0,0,1]]
         '''
+        
         self.QueueSizes = []
         self.sumQueueSizes = []
         self.NetworkOccupancy = 0
@@ -104,6 +114,7 @@ class Graph:
         queueSizesTemp = []
         for node in self.Nodes:
             queueSizesTemp.append(node.QueueSize)
+        queueSizesTemp.pop()
         self.sumQueueSizes.append(sum(queueSizesTemp))
         self.QueueSizes.append(queueSizesTemp)
         self.Time += 1 
@@ -116,47 +127,127 @@ class Graph:
                 minQueueSize = self.Nodes[nodeId].QueueSize
         return minQueueNode
 
-lam = 0.8
+lam = 0.7
 
-lambdaVSNArray1 = [];
+x_values = []
+y_values_1t = []
+y_values_1f = []
+y_values_2t = []
+y_values_2f = []
+y_values_3t = []
+y_values_3f = []
 
-j = 0.8
+edges1 = [[1,0.4,0.4,0.4,0,0,0,0,0],
+          [0,1,0,0,0.4,0.4,0,0,0],
+          [0,0,1,0.4,0.4,0.4,0,0,0],
+          [0,0,0,1,0,0,0,0.4,0],
+          [0,0,0,0,1,0,0.4,0.4,0],
+          [0,0,0,0,0,1,0.4,0.4,0.4],
+          [0,0,0,0,0,0,1,0,0.4],
+          [0,0,0,0,0,0,0,1,0.4],
+          [0,0,0,0,0,0,0,0,1]]
+
+edges2 = [[1, 0.4, 0, 0.4, 0, 0, 0],
+          [0, 1, 0.4, 0.4, 0.1, 0, 0],
+          [0, 0, 1, 0, 0.4, 0, 0],
+          [0, 0, 0, 1, 0.4, 0.1, 0],
+          [0, 0, 0, 0, 1, 0.4, 0.1],
+          [0, 0, 0, 0, 0, 1, 0.4],
+          [0, 0, 0, 0, 0, 0, 1]]
+
+edges3 = [[1,0.5,0.5,0.5,0,0,0,0,0,0,0],
+          [0,1,0,0,0.5,0.5,0.5,0,0,0,0],
+          [0,0,1,0,0.5,0.5,0.5,0,0,0,0],
+          [0,0,0,1,0.5,0.5,0.5,0,0,0,0],
+          [0,0,0,0,1,0,0,0.5,0.5,0.5,0],
+          [0,0,0,0,0,1,0,0.5,0.5,0.5,0],
+          [0,0,0,0,0,0,1,0.5,0.5,0.5,0],
+          [0,0,0,0,0,0,0,1,0,0,0.5],
+          [0,0,0,0,0,0,0,0,1,0,0.5],
+          [0,0,0,0,0,0,0,0,0,1,0.5],
+          [0,0,0,0,0,0,0,0,0,0,1]]
+
+j = 0.01
 while j <= lam:
     time = 0
     n = 0
-    for i in range(10):
-        arrivals = np.random.poisson(j,10000)
+    arrivals = np.random.poisson(j,1000)
 
-        graph = Graph(False)
-        for arrival in arrivals:
-            graph.Run(arrival)
-        for packet in graph.Packets:
-            if packet.IsActive:
-                n = n+1
-    n = n/10
-    #print(str(j) + "," + str(n))
-    #averageN = sum(graph.sumQueueSizes) / len(graph.sumQueueSizes)
-    #print(str(j) + "," + str(n))
-    #lambdaVSNArray1.append([j,n])
-    for i,item in enumerate(graph.sumQueueSizes):
-        lambdaVSNArray1.append([i,item]);
-    j = j + 0.1
-	
+    graph1t = Graph(edges1,True)
+    graph1f = Graph(edges1,False)
 
-#print(str(lambdaVSNArray));
-# Extract the x and y values from the data array
-x_values = [point[0] for point in lambdaVSNArray1]
-y_values = [point[1] for point in lambdaVSNArray1]
+    graph2t = Graph(edges2,True)
+    graph2f = Graph(edges2,False)
+
+    graph3t = Graph(edges3,True)
+    graph3f = Graph(edges3,False)
+
+    for arrival in arrivals:
+        graph1t.Run(arrival)
+        graph1f.Run(arrival)
+        graph2t.Run(arrival)
+        graph2f.Run(arrival)
+        graph3t.Run(arrival)
+        graph3f.Run(arrival)
+
+    y_values_temp_1t = 0
+    y_values_temp_1f = 0
+    y_values_temp_2t = 0
+    y_values_temp_2f = 0
+    y_values_temp_3t = 0
+    y_values_temp_3f = 0
+    for i in range(len(graph1t.sumQueueSizes)):
+        y_values_temp_1t = y_values_temp_1t + graph1t.sumQueueSizes[i]
+        y_values_temp_1f = y_values_temp_1f + graph1f.sumQueueSizes[i]
+
+        y_values_temp_2t = y_values_temp_2t + graph2t.sumQueueSizes[i]
+        y_values_temp_2f = y_values_temp_2f + graph2f.sumQueueSizes[i]
+
+        y_values_temp_3t = y_values_temp_3t + graph3t.sumQueueSizes[i]
+        y_values_temp_3f = y_values_temp_3f + graph3f.sumQueueSizes[i]
+
+    x_values.append(j)
+    y_values_1t.append(y_values_temp_1t/1000)
+    y_values_1f.append(y_values_temp_1f/1000)
+    y_values_2t.append(y_values_temp_2t/1000)
+    y_values_2f.append(y_values_temp_2f/1000)
+    y_values_3t.append(y_values_temp_3t/1000)
+    y_values_3f.append(y_values_temp_3f/1000)
+    j = j+0.01
 
 # Create a scatter plot
-plt.scatter(x_values, y_values)
 
-# Add labels and title to the plot
+plt.scatter(x_values,y_values_1t,color='red',label="Queue Selection")
+plt.scatter(x_values,y_values_1f,color='blue',label="Index Selection")
+
 plt.xlabel('Lambda')
 plt.ylabel('N')
-plt.title('Lambda vs N from 0 to ' + str(lam))
+plt.title('N vs Lambda from 0.01 to 0.6 for Graph 1')
 
-# Display the plot
+plt.legend()
+plt.grid(True)
+plt.show()
+
+plt.scatter(x_values,y_values_2t,color='red',label="Queue Selection")
+plt.scatter(x_values,y_values_2f,color='blue',label="Index Selection")
+
+plt.xlabel('Lambda')
+plt.ylabel('N')
+plt.title('N vs Lambda from 0.01 to 0.6 for Graph 2')
+
+plt.legend()
+plt.grid(True)
+plt.show()
+
+plt.scatter(x_values,y_values_3t,color='red',label="Queue Selection")
+plt.scatter(x_values,y_values_3f,color='blue',label="Index Selection")
+
+plt.xlabel('Lambda')
+plt.ylabel('N')
+plt.title('N vs Lambda from 0.01 to 0.6 for Graph 3')
+
+plt.legend()
+plt.grid(True)
 plt.show()
 
 
